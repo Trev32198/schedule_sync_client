23de5ef417e1021f2692df74ce9b4d564b509ffe
Trev32198 <tduma001@plattsburgh.edu>
2020-05-14 00:48:22 -0400

    Fix text truncation and make zoom expansion look nicer

--- app/src/main/java/com/example/myapplication/ExpandZoomEvent.java
+++ app/src/main/java/com/example/myapplication/ExpandZoomEvent.java
@@ -1,10 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ExpandZoomEvent extends AppCompatActivity {
 
@@ -16,25 +16,20 @@ public class ExpandZoomEvent extends AppCompatActivity {
         Intent intent = getIntent();
         ZoomEvent zoomEvent = intent.getParcelableExtra("Zoom Event");
 
-        TextView zoomTitle, zoomCourse, zoomMonth, zoomDay, zoomYear, zoomHour, zoomMinute, zoomCode;
+        TextView zoomTitle, zoomCourse, zoomTime, zoomCode;
 
         zoomTitle = findViewById(R.id.zoomTitle);
         zoomCourse = findViewById(R.id.zoomCourse);
-        zoomMonth = findViewById(R.id.zoomMonth);
-        zoomDay = findViewById(R.id.zoomDay);
-        zoomYear = findViewById(R.id.zoomYear);
-        zoomHour = findViewById(R.id.zoomHour);
-        zoomMinute = findViewById(R.id.zoomMinute);
+        zoomTime = findViewById(R.id.zoomTime);
         zoomCode = findViewById(R.id.zoomCode);
 
         assert zoomEvent != null;
         zoomTitle.setText(String.format("Title: %s", zoomEvent.getTitle()));
         zoomCourse.setText(String.format("Course: %s", zoomEvent.getCourse()));
-        zoomMonth.setText(String.format("Month: %s", String.valueOf(zoomEvent.getMonth())));
-        zoomDay.setText(String.format("Day: %s", String.valueOf(zoomEvent.getDay())));
-        zoomYear.setText(String.format("Year: %s", String.valueOf(zoomEvent.getYear())));
-        zoomHour.setText(String.format("Hour: %s", String.valueOf(zoomEvent.getHour())));
-        zoomMinute.setText(String.format("Minute: %s", String.valueOf(zoomEvent.getMinute())));
+        zoomTime.setText(String.format("When: %s at %s",
+                zoomEvent.getYear() + "-" + zoomEvent.getMonth() + "-" + zoomEvent.getDay(),
+                zoomEvent.getHour() + ":" + (zoomEvent.getMinute() < 10 ? "0" + zoomEvent.getMinute() : zoomEvent.getMinute())));
+
         zoomCode.setText(String.format("Zoom Code: %s", zoomEvent.getRoomCode()));
     }
 }
